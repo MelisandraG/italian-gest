1ed34ed69424ae6b09b30fdea9c182461072720e
Francisco Pereira <fmcp31@gmail.com>
2022-03-19 19:22:17 +0000

    Add the nav bar

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+// NAVBAR JS
+
+
+let mobileNav = document.querySelector(".mobileNav");
+let hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click" , function() {
+  mobileNav.classList.toggle("isActive");
+  hamburger.classList.toggle("isActive");
+});
+
+
+// END OF NAVBAR JS
